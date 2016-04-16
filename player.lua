6f20ce9fd4a87b96dd9866f79e369a24f1d05c61
Hamdan Javeed <hamdan@hamdanjaveed.com>
2016-04-16 17:07:02 -0400

    Add a player velocity

--- player.lua
+++ player.lua
@@ -1,6 +1,7 @@
 local Anim8 = require('modules/anim8/anim8')
 local Class = require('modules/middleclass/middleclass')
 local Stateful = require('modules/stateful/stateful')
+local vector = require('modules/hump/vector')
 
 local Player = Class('Player')
 Player:include(Stateful)
@@ -14,8 +15,8 @@ local sprites = {
 
 --============================================================================== PLAYER
 function Player:initialize(x, y)
-    self.x = x
-    self.y = y
+    self.pos = vector(x, y)
+    self.vel = vector(10, -10)
 
     local grid = Anim8.newGrid(Player.SIZE, Player.SIZE, Player.SIZE * 6, Player.SIZE)
     self.animation = Anim8.newAnimation(grid:getFrames('1-6', 1), 0.05)
@@ -25,11 +26,12 @@ function Player:initialize(x, y)
 end
 
 function Player:update(dt)
+	self.vel.y = self.vel.y + 9.8 * dt
     self.animation:update(dt)
 end
 
 function Player:draw()
-    self.animation:draw(self.sprite, self.x, self.y, 0, 1, 1, Player.SIZE / 2, Player.SIZE / 2)
+    self.animation:draw(self.sprite, self.pos.x, self.pos.y, 0, 1, 1, Player.SIZE / 2, Player.SIZE / 2)
 end
 
 --============================================================================== PLAYER.BALL
